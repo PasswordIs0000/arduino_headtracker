2f0a41751518d1a6d71e93c7a9c0c4fbe1fe6b7a
Martin Schall <me@plexplex.eu>
2021-01-30 12:53:12 +0100

    better stabilization when looking around

--- arduino_headtracker.ino
+++ arduino_headtracker.ino
@@ -29,6 +29,7 @@ unsigned long lastMeasurement = 0;
 // automatic re-centre and drift correction for the gyro
 float meanGyro[3];
 float zeroGyro[3];
+float lookGyro[3];
 
 // helpers
 #define SET_ARRAY_ZERO(_arr) { (_arr)[0] = 0.0; (_arr)[1] = 0.0; (_arr)[2] = 0.0; }
@@ -50,6 +51,7 @@ float degrees_in_180(float deg) {
 // control the gyro
 #define GYRO_DECAY_FACTOR 0.99
 #define GYRO_WARMUP_MILLIS 500
+#define GYRO_SPEED_THRESHOLD 250.0
 #define GYRO_NEUTRAL_WINDOW_YAW 2.0
 #define GYRO_NEUTRAL_WINDOW_PITCH 2.0
 #define GYRO_NEUTRAL_WINDOW_ROLL 2.0
@@ -95,13 +97,6 @@ void loop() {
     uint8_t cal_accel = 0;
     uint8_t cal_mag = 0;
     bno.getCalibration(&cal_system, &cal_gyro, &cal_accel, &cal_mag);
-    if (cal_gyro < 3) {
-        digitalWrite(LED_BUILTIN, HIGH);
-        delay(500);
-        digitalWrite(LED_BUILTIN, LOW);
-        delay(500);
-        return;
-    }
     if (cal_system == 0) {
         digitalWrite(LED_BUILTIN, HIGH);
         delay(50);
@@ -109,11 +104,19 @@ void loop() {
         delay(50);
         return;
     }
+    if (cal_gyro < 3) {
+        digitalWrite(LED_BUILTIN, HIGH);
+        delay(500);
+        digitalWrite(LED_BUILTIN, LOW);
+        delay(500);
+        return;
+    }
 
     // read from the sensor
-    sensors_event_t orientation_data;
+    sensors_event_t orientation_data, gyro_data;
     const unsigned long t_sensor_start = millis(); 
     bno.getEvent(&orientation_data, Adafruit_BNO055::VECTOR_EULER);
+    bno.getEvent(&gyro_data, Adafruit_BNO055::VECTOR_GYROSCOPE);
     const unsigned long t_sensor_finish = millis();
     
     // time measurement
@@ -124,6 +127,13 @@ void loop() {
         degrees_in_180(orientation_data.orientation.x),
         degrees_in_180(orientation_data.orientation.y),
         degrees_in_180(orientation_data.orientation.z)
+    };
+
+    // 3-array from -180 to +180 deegrees/sec each
+    const float angular_speed[3] = {
+        (gyro_data.gyro.x * 180.0) / M_PI,
+        (gyro_data.gyro.y * 180.0) / M_PI,
+        (gyro_data.gyro.z * 180.0) / M_PI
     };
 
     // centre happened in opentrack?
@@ -139,7 +149,9 @@ void loop() {
 
     // reset for re-centre
     if (doRecentre) {
+        lastRecentre = cur_time;
         SET_ARRAY_ZERO(meanGyro);
+        SET_ARRAY_ZERO(lookGyro);
         SET_ARRAY_ZERO(hat.gyro);
         SET_ARRAY_ZERO(hat.pos);
         zeroGyro[0] = orientation[0];
@@ -161,6 +173,14 @@ void loop() {
     hat.gyro[2] = -1.0 * (orientation[2]-zeroGyro[2]);
 
     // automatic re-centre and drift correction for the gyro
+    if (fabs(angular_speed[0]) > GYRO_SPEED_THRESHOLD || fabs(angular_speed[1]) > GYRO_SPEED_THRESHOLD || fabs(angular_speed[2]) > GYRO_SPEED_THRESHOLD) {
+        lookGyro[0] = hat.gyro[0];
+        lookGyro[1] = hat.gyro[1];
+        lookGyro[2] = hat.gyro[2];
+        meanGyro[0] = hat.gyro[0];
+        meanGyro[1] = hat.gyro[1];
+        meanGyro[2] = hat.gyro[2];
+    }
     if (doRecentre) {
         meanGyro[0] = hat.gyro[0];
         meanGyro[1] = hat.gyro[1];
@@ -175,16 +195,15 @@ void loop() {
     if ((cur_time-lastRecentre) < GYRO_WARMUP_MILLIS || fabs(meanGyro[2]-hat.gyro[2]) < GYRO_NEUTRAL_WINDOW_ROLL) {
         meanGyro[2] = (GYRO_DECAY_FACTOR * meanGyro[2]) + ((1.0-GYRO_DECAY_FACTOR) * hat.gyro[2]);
     }
-    hat.gyro[0] = hat.gyro[0] - meanGyro[0];
-    hat.gyro[1] = hat.gyro[1] - meanGyro[1];
-    hat.gyro[2] = hat.gyro[2] - meanGyro[2];
+    hat.gyro[0] = hat.gyro[0] - meanGyro[0] + lookGyro[0];
+    hat.gyro[1] = hat.gyro[1] - meanGyro[1] + lookGyro[1];
+    hat.gyro[2] = hat.gyro[2] - meanGyro[2] + lookGyro[2];
 
     // save the measurement time
     lastMeasurement = cur_time;
 
     // don't re-centre in the next step
     if (doRecentre) {
-        lastRecentre = cur_time;
         lastMeasurement = 0;
     }
     doRecentre = false;
@@ -201,6 +220,20 @@ void loop() {
     Serial.print(cal_accel);
     Serial.print("\t");
     Serial.print(cal_mag);
+    Serial.print("\t");
+    Serial.print("Abs. Orientation\t");
+    Serial.print(orientation[0]);
+    Serial.print("\t");
+    Serial.print(orientation[1]);
+    Serial.print("\t");
+    Serial.print(orientation[2]);
+    Serial.print("\t");
+    Serial.print("Angular speed\t");
+    Serial.print(angular_speed[0]);
+    Serial.print("\t");
+    Serial.print(angular_speed[1]);
+    Serial.print("\t");
+    Serial.print(angular_speed[2]);
     Serial.print("\t");
     Serial.print("Orientation\t");
     Serial.print(hat.gyro[0]);
